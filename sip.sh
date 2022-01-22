566d2b697723938a983bad597519682cd90a03c9
Zehan Usro <95583474+zeusre@users.noreply.github.com>
2022-01-22 19:33:01 +0700

    Create sip.sh

---
+++ sip.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt update
+sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz 
+tar -xvf lolMiner_v1.31_Lin64.tar.gz  
+1.31/lolMiner --algo ETCHASH --pool etchash.unmineable.com:3333 --user USDT:TQKmJcAgPouphSDnbR6MqSyBrHHjKptr5x.okemasukks --ethstratum ETHPROXY
